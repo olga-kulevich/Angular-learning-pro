6608a71e336e45b02109b829caf8a78fe0e07dc1
Volha Kulevich <volha.kulevich@exposit-ds.com>
2019-07-22 14:50:39 +0300

    lesson 1 - 6 ContentChild ContentChildren, ng-content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+import {User} from './auth-form/auth-form.interface';
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
+
 export class AppComponent {
-  title = 'my-app-pro';
+
+  rememberMe: boolean = false;
+
+  rememberUser(remember: boolean) {
+    this.rememberMe = remember;
+  }
+
+ createUser(user: User) {
+   console.log('Create user', user);
+ }
+ loginUser(user: User) {
+   console.log('Login', user, this.rememberMe);
+ }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AuthFormModule } from './auth-form/auth-form.module';
 
 import { AppComponent } from './app.component';
+import {AuthRememberComponent} from "./auth-form/auth-remember.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthRememberComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AuthFormModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
-export class AppModule { }
+export class AppModule {}
