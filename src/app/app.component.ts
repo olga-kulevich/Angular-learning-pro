6fc55e3f87e02e93cd7952a6d6da4458f7fd7c54
Volha Kulevich <volha.kulevich@exposit-ds.com>
2019-07-23 16:15:23 +0300

    lesson 21 ViewEncapsulation and ShadowDom

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,14 @@
-import {
-  Component
-} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
     <div>
-      <ng-container
-        [ngTemplateOutlet]="tmpl"
-        [ngTemplateOutletContext]="ctx"
-      >
-      </ng-container>
-      
-      <ng-template #tmpl let-name let-foo let-location="location">
-        {{name}} : {{location}}
-      </ng-template>
-      
+      <example-one></example-one>
+      <example-two></example-two>
+      <example-three></example-three>
     </div>
   `
 })
 export class AppComponent {
-  ctx = {
-    $implicit: 'Todd',
-    location: 'UK'
-  };
 }
