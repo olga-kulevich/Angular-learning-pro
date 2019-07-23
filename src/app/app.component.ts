800c5c675791b7ce5053314ead4aaa6aa29e0c0c
Volha Kulevich <volha.kulevich@exposit-ds.com>
2019-07-23 12:07:52 +0300

    lesson 15 Destroy dinamic reordering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,6 +17,9 @@ import { User } from './auth-form/auth-form.interface';
       <button (click)="destroyComponent()">
         Destroy
       </button>
+      <button (click)="moveComponent()">
+        Move
+      </button>
       <div #entry></div>
     </div>
   `
@@ -33,15 +36,19 @@ export class AppComponent implements AfterContentInit {
 
   ngAfterContentInit() {
     const authFormFactory = this.resolver.resolveComponentFactory(AuthFormComponent);
-    this.component = this.entry.createComponent(authFormFactory);
 
+    this.entry.createComponent(authFormFactory);
+
+    this.component = this.entry.createComponent(authFormFactory, 0);
     this.component.instance.title = 'Create account';
-
     this.component.instance.submitted.subscribe(this.loginUser);
   }
 
   destroyComponent() {
     this.component.destroy();
+  }
+  moveComponent() {
+    this.entry.move(this.component.hostView, 1);
   }
 
   loginUser(user: User) {
