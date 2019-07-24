6978d063a066d6069b9ec7b9d3ce90d145950dbe
Volha Kulevich <volha.kulevich@exposit-ds.com>
2019-07-24 13:27:37 +0300

    lesson 26 Property Directive exportAs for tooltip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,27 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  styleUrls: ['app.component.css'],
   template: `
     <div>
       <label>
         Credit Card Number
-        <input
+        <input 
           name="credit-card" 
           type="text"
           placeholder="Enter your 16-digit card number"
           credit-card>
+      </label>
+      <label 
+        tooltip="3 digits, back of your card"
+        #myTooltip="tooltip">
+        Enter your security code 
+        <span
+          (mouseover)="myTooltip.show()"
+          (mouseout)="myTooltip.hide()">
+          (?)
+        </span>
+        <input type="text">
       </label>
     </div>
   `
