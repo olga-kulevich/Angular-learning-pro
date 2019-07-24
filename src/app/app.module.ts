17c0044f1f1dd3e6cd081f694747c423eb761740
Volha Kulevich <volha.kulevich@exposit-ds.com>
2019-07-24 13:44:36 +0300

    lesson 27 Creating a custom structural derective

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TooltipDirective } from './tooltip/tooltip.directive';
-import { CreditCardDirective } from './credit-card/credit-card.directive';
+import { MyForDirective } from './my-for/my-for.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CreditCardDirective,
-    TooltipDirective
+    MyForDirective
   ],
   imports: [
     BrowserModule
